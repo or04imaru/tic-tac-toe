4229fb5df88a452ccf8448a1d5b8d08c20383459
Тен Андрей <andrey@Sovenok.local>
2025-12-30 17:20:47 +0500

    refactored checker function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -40,39 +40,38 @@ def players_move(board):
 
 def checker(board):
     global all_turns
+    winner = ""
 
     for row in range(3):
         if board[row][0] == board[row][1] == board[row][2]:
             winner = board[row][0]
-            return winner
 
     for column in range(3):
         if board[0][column] == board[1][column] == board[2][column]:
             winner = board[0][column]
-            return winner
 
     if board[0][0] == board[1][1] == board[2][2]:
         winner = board[0][0]
-        return winner
     elif board[0][2] == board[1][1] == board[2][0]:
         winner = board[0][2]
-        return winner
 
     if all_turns == 9:
         return "nobody"
+
+    return winner
 
 while True:
     input("press enter to play...")
     all_turns = 0
     game_over = False
-    board = initialize_board()
-    show_board(board)
+    game_board = initialize_board()
+    show_board(game_board)
 
     while not game_over:
-        players_move(board)
-        show_board(board)
+        players_move(game_board)
+        show_board(game_board)
 
-        winner = checker(board)
-        if winner:
+        victor = checker(game_board)
+        if victor:
             game_over = True
-            print(winner, "is the winner", "\n")
+            print(victor, "is the winner", "\n")
